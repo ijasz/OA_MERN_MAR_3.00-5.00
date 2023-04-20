3df340d49b5ee51f2813ea25822832f2473d2ee5
“ijasz” <jass07rtr@gmail.com>
2023-04-20 17:21:47 +0530

    string methods

---
+++ JS/string-methods/index.js
@@ -0,0 +1,22 @@
+// let text1 = "sea";
+// let text2 = "food";
+// let result = text1.concat(" ", text2, " ", "is good");
+
+// console.log(result);
+
+// console.log(text1 + " " + text2);
+
+// let text = "Hello world";
+// let result = text.endsWith("world");
+
+// console.log(result);
+
+// let text = "Hello world, welcome to the universe.";
+// let result = text.includes("world");
+
+// console.log(result);
+
+let text = "W3Schools";
+let character = text.at();
+
+console.log(character);
