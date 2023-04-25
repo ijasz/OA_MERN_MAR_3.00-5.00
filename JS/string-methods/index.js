6042ddf3a3b7557d4ec72df255dea022602c1c11
“ijasz” <jass07rtr@gmail.com>
2023-04-25 16:23:18 +0530

    for loop

--- JS/string-methods/index.js
+++ JS/string-methods/index.js
@@ -6,17 +6,28 @@
 
 // console.log(text1 + " " + text2);
 
-// let text = "Hello world";
-// let result = text.endsWith("world");
+let text = "Hello world";
+let result = text.endsWith("lo", 5);
 
-// console.log(result);
+console.log(result);
 
 // let text = "Hello world, welcome to the universe.";
 // let result = text.includes("world");
 
 // console.log(result);
 
-let text = "W3Schools";
-let character = text.at();
+// let text = "W3Schools";
+// let character = text.at();
 
-console.log(character);
+// console.log(character);
+
+const sentence = "The quick brown fox jumps over the lazy dog.";
+
+const word = "fox";
+
+console.log(
+  `The word "${word}" ${
+    sentence.includes(word) ? "is" : "is not"
+  } in the sentence`
+);
+// Expected output: "The word "fox" is in the sentence"
