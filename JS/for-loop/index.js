b9e760a2f59565ca6999f5e3096e5c398e68c95b
“ijasz” <jass07rtr@gmail.com>
2023-04-25 15:43:11 +0530

    for of , for nesting

---
+++ JS/for-loop/index.js
@@ -0,0 +1,34 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// const arr = [1, 2, 3, 434, 54, 6];
+
+// for (const i of arr) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// const obj = {
+//   f: "ocean",
+//   l: "academy",
+//   age: 10,
+// };
+
+// console.log(obj.age);
+// console.log(obj["age"]);
+
+// for (const key in obj) {
+//   console.log(`key : ${key}`);
+//   console.log(`value: ${obj[key]}`);
+//   console.log("");
+// }
+
+// for (let i = 0; i <= 3; i++) {
+//   for (let j = 0; j <= 3; j++) {
+//     console.log(i, j);
+//   }
+// }
