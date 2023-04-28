20327234e757c4bc93f569ce06aaa608f089657a
“ijasz” <jass07rtr@gmail.com>
2023-04-28 15:59:14 +0530

    last update

---
+++ JS/while-loop/index.js
@@ -0,0 +1,19 @@
+// let i = 0;
+
+// while (i < 10) {
+//   console.log("hello", ++i);
+// }
+
+// do {
+//   console.log("hello", ++i);
+// } while (i < 10);
+
+// let output = "";
+// let a = 123;
+// a = a.toString();
+
+// for (let i = a.length - 1; i >= 0; i--) {
+//   output += a[i];
+// }
+
+// console.log(output);
