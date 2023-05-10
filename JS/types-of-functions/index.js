6964c61c39906c56664b8d9373a45a10ed80eeee
“ijasz” <jass07rtr@gmail.com>
2023-05-10 15:52:42 +0530

    types of function

---
+++ JS/types-of-functions/index.js
@@ -0,0 +1,55 @@
+// Function Statement
+
+// function greeting() {
+//   console.log("hello sunil");
+// }
+
+// greeting();
+
+// Function Expression
+
+// let a = function greeting() {
+//   console.log("hello sunil");
+// };
+
+// Named Function
+
+// let a = function greeting() {
+//   console.log("hello sunil");
+// };
+
+// a();
+
+// anonymous Function
+
+// let a = function () {
+//   console.log("hello sunil");
+// };
+
+// a();
+
+// Arrow Function
+
+// let a = () => console.log("hello sunil");
+
+// a();
+
+// IIFE
+
+// (() => {
+//   console.log("IIFE");
+// })();
+
+// First Class Function ?
+
+// Higher Order Function
+
+// function greet(name) {
+//   console.log(name + " " + "hello");
+// }
+
+// function sayHello(c) {
+//   c("sunil");
+// }
+
+// sayHello(greet);
