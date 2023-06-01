ac56409c8cc470421624f88c6b79858676d0edbd
“ijasz” <jass07rtr@gmail.com>
2023-06-01 16:05:12 +0530

    express http methods update

--- Express/http-methods/index.js
+++ Express/http-methods/index.js
@@ -2,23 +2,11 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 const PORT = 5000;
-const students = [];
+const data = require("./data");
+const students = data;
 
+// middleware
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("<h1>welcome to node express api</h1>");
-});
-
-app.get("/get", (req, res) => {
-  if (students.length > 0) {
-    console.log("if");
-    res.status(200).send(students);
-  } else {
-    console.log("else");
-    res.status(204).send(students);
-  }
-});
 
 app.post("/addOne", (req, res) => {
   const uniqueId = uuidv4();
@@ -40,6 +28,44 @@ app.post("/addMany", (req, res) => {
   }
 
   res.status(204).send("please add any data to post");
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>welcome to node express api</h1>");
+});
+
+app.get("/get", (req, res) => {
+  if (students.length > 0) {
+    res.status(200).send(students);
+  } else {
+    res.status(204).send(students);
+  }
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const len = students.length;
+  for (let i = 0; i < len; i++) {
+    if (students[i].id === Number(id)) {
+      return res.send(students[i]);
+    }
+  }
+  res.status(404).send("id is not found");
+});
+
+app.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+  const len = students.length;
+
+  for (let i = 0; i < len; i++) {
+    if (students[i].id === Number(id)) {
+      students[i] = { ...students[i], ...updateData };
+      return res.status(200).send("updated successfully");
+    }
+  }
+
+  res.status(404).send("id is not found");
 });
 
 app.listen(PORT, () => {
